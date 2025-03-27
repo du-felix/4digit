6e79d5ea4e5237d5792be9837d898e13a33de34b
du-felix <dufelix007@gmail.com>
2025-03-27 01:49:26 +0100

    changed mails

--- antraege/functions.py
+++ antraege/functions.py
@@ -6,17 +6,17 @@ from django.urls import reverse
 
 def send_email(email, vorname, schueler, unterricht,token_url):
     subject = "Neuer Freistellungsantrag"
-    body = f"""Hallo {vorname},\n ihr Schüler {schueler} hat einen Antrag gestellt. Folgende Stunden sind davon betroffen:\n\n
+    body = f"""Hallo {vorname.capitalize()},\n ihr Schüler {schueler} hat einen Antrag gestellt. Folgende Stunden sind davon betroffen:\n\n
     {unterricht}
-        \n\n Bitte bearbeiten sie den Antrag in nächster Zeit. Über folgenden Link können Sie den Antrag bearbeiten: {token_url}\n\n Vielen Dank! BITTE ANTWORTEN SIE NICHT AUF DIESE MAIL!
+        \n\n Bitte bearbeiten sie den Antrag in nächster Zeit. Über folgenden Link können Sie den Antrag bearbeiten: {token_url}\n\nVielen Dank! BITTE ANTWORTEN SIE NICHT AUF DIESE MAIL!
     """
     empfaenger = [email]
     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, empfaenger, fail_silently=False)
 
 def send_email_gm(email, vorname, schueler, token_url):
     subject = "Neuer Freistellungsantrag"
-    body = f"""Hallo {vorname},\n ihr Mentee {schueler} hat einen Antrag gestellt.
-        \n\n Bitte bearbeiten sie den Antrag in nächster Zeit. Über folgenden Link können Sie den Antrag bearbeiten: {token_url}\n\n Vielen Dank! BITTE ANTWORTEN SIE NICHT AUF DIESE MAIL!
+    body = f"""Hallo {vorname.capitalize()},\n ihr Mentee {schueler} hat einen Antrag gestellt.
+        \n\n Bitte bearbeiten sie den Antrag in nächster Zeit. Über folgenden Link können Sie den Antrag bearbeiten:\n\n{token_url}\n\nVielen Dank! BITTE ANTWORTEN SIE NICHT AUF DIESE MAIL!
     """
     empfaenger = [email]
     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, empfaenger, fail_silently=False)
@@ -24,7 +24,7 @@ def send_email_gm(email, vorname, schueler, token_url):
 def send_email_schulleiter(schueler, token_url):
     subject = "Neuer Freistellungsantrag"
     body = f"""Hallo Stefan,\n der Schüler {schueler} hat einen Antrag gestellt.
-        \n\n Bitte bearbeiten sie den Antrag in nächster Zeit. Über folgenden Link können Sie den Antrag bearbeiten: {token_url}\n\n Vielen Dank! BITTE ANTWORTEN SIE NICHT AUF DIESE MAIL!
+        \n\n Bitte bearbeiten sie den Antrag in nächster Zeit. Über folgenden Link können Sie den Antrag bearbeiten:\n\n{token_url}\n\nVielen Dank! BITTE ANTWORTEN SIE NICHT AUF DIESE MAIL!
     """
     empfaenger = ["stefan.weih@afra.lernsax.de"]
     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, empfaenger, fail_silently=False)
